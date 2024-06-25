31e844dcb8559b8d025d64d467bc1394d285a44f
Nagulapally Deepa <145601800+draftergithub@users.noreply.github.com>
2024-06-25 10:16:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// You can add JavaScript for any interactive elements here
+// Example: Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
